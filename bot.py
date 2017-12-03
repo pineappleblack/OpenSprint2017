4e62020d6d3bb9473292a1b2ff737acc31ac8738
Alexey Smagin <asblackpn@gmail.com>
2017-12-03 12:51:48 +0300

    bot added

---
+++ bot.py
@@ -0,0 +1,14 @@
+﻿import telebot
+
+token = '479204960:AAHVGrW6jzi1xSegLtAFOlpII9ouy7tkJFg'
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(content_types=["text"])
+def repeat_all_messages(message):
+    answer = "Спасибо за внимание ко мне. Я польщён из локалки"
+    bot.send_message(message.chat.id, answer)
+
+if __name__ == '__main__':
+    print('It works!')
+    bot.polling(none_stop=True)
